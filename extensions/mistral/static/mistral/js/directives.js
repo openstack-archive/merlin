525477601b6079a042751d04693f14db343fce40
Timur Sufiev <tsufiev@mirantis.com>
2014-12-29 07:31:08 -0800

    Replace combined field (YF + AF) with angular directive
    Change-Id: Ie8ba632c1bb6a9a47c427c92aa620b7dd7c28485

--- extensions/mistral/static/mistral/js/directives.js
+++ extensions/mistral/static/mistral/js/directives.js
@@ -1,21 +1,55 @@
 /**
  * Created by tsufiev on 12/29/14.
  */
-  angular.module('hz').directive('editable', function($document) {
-    return {
-      restrict: 'E',
-      templateUrl: '/static/mistral/js/angular-templates/editable-popup.html',
-      scope: {
-        label: '@',
-        value: '@'
-      },
-      link: function(scope, element) {
-        angular.element(element).find('a[data-toggle="popover"]')
-          .popover({html: true})
-          .on('click', function(e) {
-            e.preventDefault();
-            return true;
-          });
+
+(function() {
+  angular.module('hz')
+
+    .directive('editable', function() {
+      return {
+        restrict: 'E',
+        templateUrl: '/static/mistral/js/angular-templates/editable-popup.html',
+        scope: {
+          label: '@',
+          value: '@'
+        },
+        link: function(scope, element) {
+          angular.element(element).find('a[data-toggle="popover"]')
+            .popover({html: true})
+            .on('click', function(e) {
+              e.preventDefault();
+              return true;
+            });
+        }
+      };
+    })
+
+    .directive('yaqlFieldCombined', function() {
+      return {
+        restrict: 'E',
+        templateUrl: '/static/mistral/js/angular-templates/yaql-field-combined.html',
+        scope: {
+          yaqlExpression: '@',
+          value: '@'
+        },
+        link: function(scope, element) {
+          angular.element(element).find('span.yaql-condition')
+            .on('click', function() {
+              var $elt = $(this),
+                $inputColumn = $elt.closest('div.row').children(':first-child'),
+                $input;
+
+              $elt.hide();
+              $input = $inputColumn.show().find('textarea');
+              $input.focus().on('blur', function() {
+                $inputColumn.hide();
+                $elt.toggleClass('fa-lock', $input.val() !== '');
+                $elt.toggleClass('fa-unlock', $input.val() === '');
+                $elt.show();
+              });
+            });
+        }
       }
-    };
-  });
+    })
+
+})();
