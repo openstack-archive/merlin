ba8b1347ce20160a8ca059a8768976cb7ff46fd8
Timur Sufiev <tsufiev@mirantis.com>
2014-12-31 07:53:49 -0800

    Implement data-driven layout for Actions
    First idea was to add Action schema to the scope, but it lead to
    serious performance issues: when any value on the scope changes, all
    the widgets which layout depends on it are redrawn, which introduces
    form flickering and high latency. Thus schema had to be pushed to the
    service, enabling its injection at the directive compile phase (it is
    evaluated only once, contrary to the link phase).
    
    Change-Id: I8e2513c5aeabd8ec4bbf72ed025bd3d5da08f94d

--- extensions/mistral/static/mistral/js/controllers.js
+++ extensions/mistral/static/mistral/js/controllers.js
@@ -5,63 +5,118 @@
 (function() {
   angular.module('hz')
 
-    .controller('actionsCtrl', function($scope) {
-      $scope.actions = [
-        [{
-          name: 'Name',
-          type: 'string',
-          value: 'Action1'
-        }, {
-          name: 'Base',
-          type: 'string',
-          value: 'nova.create_server'
-        }, {
-          name: 'Base Input',
-          type: 'fixedlist'
-        }, {
-          name: 'Input',
-          type: 'list',
-          value: [{
-            type: 'string',
-            value: ''
-          }]
-        }, {
-          name: 'Output',
-          type: 'varlist',
-          value: [{
-            type: 'string',
-            value: ''
-          }, {
-            type: 'dictionary',
-            value: [{
-              name: 'key1',
+    .controller('workbookCtrl', function($scope) {
+      $scope.data = {
+        actions: [{
+          name: 'Action1',
+          base: 'nova.create_server',
+          baseInput: {
+            flavorId: {
+              title: 'Flavor Id',
+              type: 'string'
+            },
+            imageId: {
+              title: 'Image Id',
+              type: 'string'
+            }
+          },
+          input: [''],
+          output: [{
               type: 'string',
               value: ''
             }, {
-              name: 'key2',
-              type: 'string',
-              value: ''
-            }]
-          }, {
-            type: 'list',
-            value: [{
-              type: 'string',
-              value: ''
+              type: 'dictionary',
+              value: {
+                key1: '',
+                key2: ''
+              }
             }, {
-              type: 'string',
-              value: ''
+              type: 'list',
+              value: ['', '']
             }]
-          }]
-        }]];
-
-      $scope.baseTypes = {
-        'nova.create_server': [{
-          name: 'Flavor Id',
-          type: 'string'
-        }, {
-          name: 'Image Id',
-          type: 'string'
         }]
+      };
+
+      $scope.schema = {
+        action: [{
+          name: 'name',
+          type: 'string',
+          group: 'one'
+        }, {
+          name: 'base',
+          type: 'string',
+          group: 'one'
+        }, {
+          name: 'baseInput',
+          type: 'frozendict',
+          group: ''
+        }, {
+          name: 'input',
+          type: 'list',
+          group: ''
+        }, {
+          name: 'output',
+          type: 'varlist',
+          group: ''
+        }
+        ]
+      };
+
+      $scope.makeTitle = function(str) {
+        var firstLetter = str.substr(0, 1).toUpperCase();
+        return firstLetter + str.substr(1);
+      };
+
+      $scope.getKeys = function(obj) {
+        return Object.keys(obj);
+      };
+
+
+    })
+
+    .controller('actionCtrl', function($scope) {
+      $scope.fixedFields = [['name', 'base']];
+      $scope.fields = ['baseInput', 'input', 'output'];
+
+      var actionBase = null,
+        baseTypes = {
+          'nova.create_server': {
+            flavorId: {
+              title: 'Flavor Id',
+              type: 'string'
+            },
+            imageId: {
+              title: 'Image Id',
+              type: 'string'
+            }
+          }
+        };
+
+
+
+      //$scope.getBaseInput = function() {
+      //  return baseTypes[actionBase] || [];
+      //};
+      //
+      //$scope.updateBase = function(newBase) {
+      //  actionBase = newBase;
+      //  var values = [];
+      //  angular.forEach($scope.getBaseInput(), function(item) {
+      //    values.push(item.value || '');
+      //  });
+      //  $scope.item.baseInput.value = values;
+      //}
+    })
+
+    .controller('dictionaryCtrl', function($scope) {
+      if ( !$scope.item.value ) {
+        $scope.item.value = {'Key1': ''};
+      }
+    })
+
+    .controller('listCtrl', function($scope) {
+      if ( !$scope.item.value ) {
+        $scope.item.value = [''];
       }
     })
 
--- extensions/mistral/static/mistral/js/directives.js
+++ extensions/mistral/static/mistral/js/directives.js
@@ -11,6 +11,20 @@
       });
   }
 
+  function makeFieldDirective(type) {
+    return function() {
+      return {
+        restrict: 'A',
+        scope: true,
+        templateUrl: '/static/mistral/js/angular-templates/fields/' + type + '.html',
+        link: function (scope, element, attrs) {
+          scope.name = attrs.name;
+          scope.title = attrs.title;
+        }
+      };
+    };
+  }
+
   angular.module('hz')
 
     .directive('editable', function() {
@@ -19,7 +33,7 @@
         templateUrl: '/static/mistral/js/angular-templates/editable-popup.html',
         scope: {
           label: '@',
-          value: '@'
+          value: '='
         },
         link: function(scope, element) {
           angular.element(element).find('a[data-toggle="popover"]')
@@ -67,20 +81,19 @@
       }
     })
 
-    .directive('collapsiblePanel', function($parse, idGenerator, defaultSetter) {
+    .directive('collapsiblePanel', function($parse, defaultSetter) {
       return {
         restrict: 'E',
         templateUrl: '/static/mistral/js/angular-templates/collapsible-panel.html',
         transclude: true,
         scope: {
-          panelTitle: '@',
+          title: '@',
           removable: '@'
         },
         compile: function(element, attrs) {
           defaultSetter(attrs, 'removable', false);
           return {
-            post: function(scope, element) {
-              scope.panelId = idGenerator();
+            post: function(scope, element, attrs) {
               disableClickDefaultBehaviour(element);
             }
           }
@@ -88,13 +101,13 @@
       }
     })
 
-    .directive('collapsibleGroup', function($parse, idGenerator, defaultSetter) {
+    .directive('collapsibleGroup', function($parse, defaultSetter) {
       return {
         restrict: 'E',
         templateUrl: '/static/mistral/js/angular-templates/collapsible-group.html',
         transclude: true,
         scope: {
-          groupTitle: '@',
+          title: '@',
           additive: '@',
           removable: '@'
         },
@@ -103,9 +116,59 @@
           defaultSetter(attrs, 'additive', false);
           return {
             post: function(scope, element) {
-              scope.groupId = idGenerator();
               disableClickDefaultBehaviour(element);
             }
+          }
+        }
+      }
+    })
+
+    .directive('dictionaryField', makeFieldDirective('dictionary'))
+    .directive('frozendictField', makeFieldDirective('frozendict'))
+    .directive('listField', makeFieldDirective('list'))
+    .directive('stringField', makeFieldDirective('string'))
+    .directive('varlistField', makeFieldDirective('varlist'))
+
+    .directive('action', function($compile, $filter, $interpolate, schema, isAtomic) {
+      var orderBy = $filter('orderBy'),
+        toArray = $filter('toArray'),
+        groupBy = $filter('groupBy');
+      return {
+        restrict: 'E',
+        compile: function(tElement, tAttrs) {
+          angular.forEach(
+            orderBy(toArray(groupBy(schema.action, 'group'), true), '-$key'),
+            function(specs) {
+              var group = specs.$key,
+                cls = group ? 'class="three-columns" ' : '',
+                $groupElt = angular.element('<div ' + cls + '></div>');
+              tElement.append($groupElt);
+              angular.forEach(specs, function(spec, index) {
+                var cls = '',
+                  separator = index % 2 ? '<div class="clearfix"></div>' : '',
+                  elt;
+                if ( isAtomic(spec.type) ) {
+                  cls = index % 2 ? 'right-column' : 'left-column';
+                }
+                elt = $interpolate(
+                  '<div class="{$ cls $}">' +
+                  '<div {$ type $}-field="{$ type $}" title="{$ title $}" name="{$ name $}">' +
+                  '{$ columnsSeparator $}</div></div>'
+                )({
+                  cls: cls, type: spec.type, name: spec.name, title: spec.title,
+                  columnsSeparator: separator
+                });
+                $groupElt.append(elt);
+              })
+            });
+          var linkFns = [];
+          tElement.children().each(function(tElem) {
+            linkFns.push($compile(tElem));
+          });
+          return function(scope) {
+            linkFns.forEach(function(linkFn) {
+              linkFn(scope);
+            })
           }
         }
       }
