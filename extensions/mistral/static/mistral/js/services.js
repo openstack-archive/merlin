ac11977d988f287e29076bd213be8f0432bae733
Timur Sufiev <tsufiev@mirantis.com>
2015-01-15 20:53:04 +0300

    Move an entire workbook to schema.
    This is a preparation for integrating with Barricade.js
    
    Change-Id: Iba9424ddc8337497d5bb8431c114b1b07902f6b8

--- extensions/mistral/static/mistral/js/services.js
+++ extensions/mistral/static/mistral/js/services.js
@@ -16,7 +16,8 @@
     })
 
     .run(function($http, $templateCache) {
-      var fields = ['dictionary', 'frozendict', 'list', 'string', 'varlist'];
+      var fields = ['dictionary', 'frozendict', 'list', 'string',
+        'varlist', 'text', 'group', 'yaqllist'];
       fields.forEach(function(field) {
         var base = '/static/mistral/js/angular-templates/fields/';
         $http.get(base + field + '.html').success(function(templateContent) {
@@ -24,5 +25,48 @@
         });
       })
     })
+
+    .factory('workbook', function() {
+      //return types.Mistral.Workbook.create();
+      return {};
+    })
+
+    .filter('prepareSchema', function($filter) {
+      var toArray = $filter('toArray'),
+        orderBy = $filter('orderBy');
+      function schemaToArray(schema) {
+        return angular.isArray(schema) ? schema : orderBy(
+          toArray(schema, true), 'index').map(function(item) {
+            item.name = item.$key;
+            if ( item.type === 'panel' ) {
+              item.panelIndex = item.index;
+            }
+            if ( item.type === 'panel' || item.type === 'group' ) {
+              item.value = schemaToArray(item.value);
+            }
+            return item;
+          });
+      }
+      return schemaToArray;
+    })
+
+    .filter('normalizePanels', function() {
+      return function(collection) {
+        return collection.map(function(panelSpec) {
+          if ( panelSpec[0].type === 'panel' ) {
+            var data = panelSpec[0];
+            panelSpec.length = data.value.length;
+            for ( var i = 0; i < panelSpec.length; i++ ) {
+              panelSpec[i] = data.value[i];
+            }
+            panelSpec.multiple = data.multiple;
+            panelSpec.name = data.name;
+          }
+          return panelSpec;
+        });
+      }
+    })
+
+
 
 })();
