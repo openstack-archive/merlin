ff432502044abcf4bac8c8998374dc978a85ebad
Paul Karikh <pkarikh@mirantis.com>
2015-01-28 13:34:03 +0300

    WIP Rewrite popup with angular
    Now popover provided with bootstrap javascript.
    It should be provided with angular.js.
    
    Change-Id: I1b23591916a8ef35751cf38c469a2065a3d0a7ce

--- extensions/mistral/static/mistral/js/directives.js
+++ extensions/mistral/static/mistral/js/directives.js
@@ -13,25 +13,89 @@
 
   angular.module('hz')
 
-    .directive('editable', function() {
+    .directive('editable', function($compile) {
       return {
-        restrict: 'E',
+        restrict: 'EAC',
         templateUrl: '/static/mistral/js/angular-templates/editable-popup.html',
         scope: {
           label: '@',
-          value: '='
+          value: '=',
+          key: '=',
+//          closePopover: '&',
+          $event: '='
         },
-        link: function(scope, element) {
-          angular.element(element).find('a[data-toggle="popover"]')
-            .popover({html: true})
+        link: function(scope, element, attrs) {
+
+          scope.closePopover = function($event){
+              console.log('closePopover');
+              var el = $event.target;
+              $($event.target).parents('div.popover').popover('hide');
+//              scope.$apply()
+          };
+          var templateData = "<label>{$ label $}</label>"+
+            "<input type='text' class='form-control' ng-model='value'>"+
+            "<button class='btn btn-default btn-sm cancel' ng-click='closePopover($event)'>Cancel</button>"+
+            "<button class='btn btn-primary btn-sm'>Save</button>";
+
+          var compiledData = $compile(templateData)(scope);
+            console.log(compiledData);
+//          angular.bind('click,function)'
+          $(element).find('a[data-toggle="popover"]')
+            .popover({html: true,
+                      content: compiledData}) //compiledData
+
             .on('click', function(e) {
+              console.log('clicked');
+              e.preventDefault();
+              return true;
+            })
+            .bind('click', function(e) {
+              console.log('clicked');
               e.preventDefault();
               return true;
             });
+
         }
       };
     })
-
+//    .directive('xngPopover', [ '$tooltip',
+//        function( $tooltip) {
+//          return $tooltip('xngPopover', 'popover', 'click');
+//        }
+//    ])
+//    .directive('xngPopoverPopup', [
+//        function() {
+//          return {
+//            replace: true,
+//            scope: { title: '@', content: '@', animation: '@', isOpen: '=' },
+//            templateUrl: '/static/mistral/js/angular-templates/editable-popup.html',
+//            controller:function($scope){
+//              $scope.Hide = function(){
+//                $scope.isOpen = false;
+//              };
+//            }
+//          };
+//        }
+//    ])
+//    .directive('popover', function($compile, $timeout){
+//      return {
+//        restrict: 'A',
+//        link:function(scope, el, attrs){
+//          var content = attrs.content; //get the template from the attribute
+//          var elm = angular.element('<div />'); //create a temporary element
+//          elm.append(attrs.content); //append the content
+//          $compile(elm)(scope); //compile
+////          alert(elm.html())
+//          //var template = $templateCache.get(scope.spec.type);
+//          //element.replaceWith($compile(template)(scope));
+//          $timeout(function() { //Once That is rendered
+//            el.removeAttr('popover').attr('data-content',elm.html()); //Update the attribute
+//            el.popover(); //set up popover
+//            alert(el.html())
+//           });
+//        }
+//      }
+//    })
     .directive('yaqlFieldCombined', function() {
       return {
         restrict: 'E',
