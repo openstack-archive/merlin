335215e9e5da404233f73d0b527d96464c3fcfc4
Timur Sufiev <tsufiev@mirantis.com>
2015-03-25 14:09:41 +0300

    Fix the bug with asynchronous template loading
    Due to each field template being loaded asynchronously it was possible
    that some templates hadn't been put to the $templateCache by the time
    they were requested from it for rendering a field directive. This lead
    to some random field shown in the initial document not being rendered
    at all. Fix this problem by using promises, effectively delaying the
    field rendering until the moment the template is finally loaded. Using
    promises also allows to not use $templateCache at all - the
    templateContents are passed as resolve() method argument.
    
    Also add 'ng-cloak' directive to the toplevel Workbook div to prevent
    raw Angular template flickering during initial load.
    
    Change-Id: I8a52b9730b52d4dd20400460137576713c081867
    Closes-Bug: #1428730

--- merlin/static/merlin/js/merlin.init.js
+++ merlin/static/merlin/js/merlin.init.js
@@ -4,17 +4,13 @@
 (function() {
   'use strict';
 
-  var merlinApp = angular.module('merlin', [])
-    .run(function($http, $templateCache) {
-      var fields = ['dictionary', 'frozendict', 'list', 'string',
-        'text', 'group', 'number', 'choices'
-      ];
-      fields.forEach(function(field) {
-        var base = '/static/merlin/templates/fields/';
-        $http.get(base + field + '.html').success(function(templateContent) {
-          $templateCache.put(field, templateContent);
-        });
-      })
-    })
+var merlinApp = angular.module('merlin', [])
+    .run(['merlin.templates', function(templates) {
+      templates.prefetch('/static/merlin/templates/fields/',
+        ['dictionary', 'frozendict', 'list', 'string', 'text', 'group', 'number',
+          'choices'
+        ]
+      );
+    }])
 
 })();
