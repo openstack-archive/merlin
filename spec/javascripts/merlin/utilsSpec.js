5a2016112d85e5f4574ff9cc4c71bcc8e1b402f9
Paul Karikh <pkarikh@mirantis.com>
2015-04-07 14:57:40 +0300

    Add karma testing
    This patch adds configuration for Karma.
    
    Change-Id: Ibb65060836f967ac81a57ea279d7fa494e238d49
    Implements blueprint: merlin-unittests

---
+++ spec/javascripts/merlin/utilsSpec.js
@@ -0,0 +1,33 @@
+describe('merlin.utils', function() {
+  'use strict';
+
+  var utils;
+
+  beforeEach(function() {
+    angular.mock.module('merlin');
+    angular.mock.inject(function($injector) {
+      utils = $injector.get('merlin.utils');
+    });
+  });
+
+  describe('makeTitle function', function() {
+    it('should capitalize the first letter of a string', function() {
+      expect(utils.makeTitle('some string')).toBe('Some string');
+    });
+  });
+
+  describe('condense Array method', function() {
+    it('Array prototype should have condense()', function() {
+      var array = [];
+      expect(array.condense).toBeDefined();
+    });
+
+    it('condense() should throw away undefined and null values', function() {
+      var array = [1, 0, 15, undefined, 7, null, null, 8];
+      expect(array.condense()).toEqual([1, 0, 15, 7, 8]);
+    });
+  });
+
+
+
+});
