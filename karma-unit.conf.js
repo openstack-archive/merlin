5af03316f7ebf142392595c34ce0321465426a11
Timur Sufiev <tsufiev@mirantis.com>
2015-04-23 21:35:51 +0300

    Add unit-tests for merlin directives
    Change-Id: If947acd2e9d7e64838c31e406d04fa9c1c67c770
    Implements: blueprint merlin-unittests

--- karma-unit.conf.js
+++ karma-unit.conf.js
@@ -15,50 +15,63 @@
  */
 
 module.exports = function (config) {
-    'use strict';
+  'use strict';
 
-    config.set({
+  config.set({
 
-        port: 9876,
+    port: 9876,
 
-        basePath: '',
+    basePath: '',
 
-        frameworks: ['jasmine'],
+    frameworks: ['jasmine'],
 
-        browsers: [ 'PhantomJS'],
+    browsers: [ 'PhantomJS'],
 
-        plugins: [
-            'karma-jasmine',
-            'karma-phantomjs-launcher',
-        ],
+    plugins: [
+      'karma-jasmine',
+      'karma-phantomjs-launcher',
+      'karma-ng-html2js-preprocessor'
+    ],
 
-        files: [
-            'bower_components/angular/angular.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            'merlin/static/merlin/js/lib/underscore-min.js',
-            'merlin/static/merlin/js/merlin.init.js',
-            'merlin/static/merlin/js/merlin.templates.js',
-            'merlin/static/merlin/js/merlin.directives.js',
-            'merlin/static/merlin/js/merlin.filters.js',
-            'merlin/static/merlin/js/merlin.field.models.js',
-            'merlin/static/merlin/js/merlin.panel.models.js',
-            'merlin/static/merlin/js/merlin.utils.js',
-            'merlin/static/merlin/js/lib/angular-filter.js',
-            'merlin/static/merlin/js/lib/barricade.js',
-            'merlin/static/merlin/js/lib/js-yaml.js',
-            'merlin/test/js/utilsSpec.js',
-            'merlin/test/js/templatesSpec.js',
-            'merlin/test/js/filtersSpec.js',
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'merlin/static/merlin/js/lib/underscore-min.js',
+      'merlin/static/merlin/js/merlin.init.js',
+      'merlin/static/merlin/js/merlin.templates.js',
+      'merlin/static/merlin/js/merlin.directives.js',
+      'merlin/static/merlin/js/merlin.filters.js',
+      'merlin/static/merlin/js/merlin.field.models.js',
+      'merlin/static/merlin/js/merlin.panel.models.js',
+      'merlin/static/merlin/js/merlin.utils.js',
+      'merlin/static/merlin/js/lib/angular-filter.js',
+      'merlin/static/merlin/js/lib/barricade.js',
+      'merlin/static/merlin/js/lib/js-yaml.js',
+      'merlin/static/merlin/templates/**/*.html',
+//      'merlin/static/merlin/templates/fields/*.html',
+      'merlin/test/js/utilsSpec.js',
+      'merlin/test/js/templatesSpec.js',
+      'merlin/test/js/filtersSpec.js',
+      'merlin/test/js/directivesSpec.js',
 
-            'extensions/mistral/static/mistral/js/mistral.init.js',
-            'extensions/mistral/static/mistral/js/mistral.workbook.models.js',
-            'extensions/mistral/static/mistral/js/mistral.workbook.controllers.js',
-            'extensions/mistral/test/js/workbookSpec.js'
-        ],
+      'extensions/mistral/static/mistral/js/mistral.init.js',
+      'extensions/mistral/static/mistral/js/mistral.workbook.models.js',
+      'extensions/mistral/static/mistral/js/mistral.workbook.controllers.js',
+      'extensions/mistral/test/js/workbookSpec.js'
+    ],
 
-        exclude: [
-        ],
+    preprocessors: {
+      'merlin/static/merlin/templates/**/*.html': ['ng-html2js']
+    },
 
-        singleRun: true
-    });
+    ngHtml2JsPreprocessor: {
+      stripPrefix: 'merlin',
+      moduleName: 'preprocessedTemplates'
+    },
+
+    exclude: [
+    ],
+
+    singleRun: true
+});
 };
