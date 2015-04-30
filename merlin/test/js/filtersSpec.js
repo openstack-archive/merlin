a56f308a9669f5eb6a2439552382234efd5ac309
Vlad Okhrimenko <vokhrimenko@mirantis.com>
2015-04-30 12:12:33 +0300

    Filter tests
    tests for filter
    
    Change-Id: I65a9676b682e2445777180c077d1e9a6b2626d97

--- merlin/test/js/filtersSpec.js
+++ merlin/test/js/filtersSpec.js
@@ -24,11 +24,11 @@ describe('merlin filters', function() {
     inject(function($injector) {
       $filter = $injector.get('$filter');
       fields = $injector.get('merlin.field.models');
-    })
+    });
   });
 
   describe('extractPanels() behavior:', function() {
-    var extractPanels, simpleMerlinObjClass;
+    var extractPanels, simpleMerlinObjClass, simpleMerlinObjClassWithMeta;
 
     beforeEach(function() {
       extractPanels = $filter('extractPanels');
@@ -42,6 +42,32 @@ describe('merlin filters', function() {
           '@type': String
         }
       });
+
+      simpleMerlinObjClassWithMeta= fields.frozendict.extend({}, {
+        '@type': Object,
+        'key1': {
+          '@type': Number,
+          '@meta': {
+                'index': 0,
+                'panelIndex': 0,
+                'row': 0
+          }
+        },
+        'key2': {
+          '@type': String,
+          '@meta': {
+                'index': 0,
+                'panelIndex': 0,
+                'row': 0,
+            '@meta': {
+                'index': 0,
+                'panelIndex': 1,
+                'row': 0
+            }
+          }
+        }
+      });
+
     });
 
     it('works properly only with objects created from Merlin classes', function() {
@@ -75,49 +101,188 @@ describe('merlin filters', function() {
       });
 
       it('the filter is applied only to the top-level entries of the passed object', function() {
+        var simpleObj = simpleMerlinObjClassWithMeta.create(),
+          panels = extractPanels(simpleObj);
 
+        expect(panels.length).toBe(1);
       });
 
     });
 
     describe('panels generated from Barricade.MutableObject (non-permanent panels)', function() {
+      var immutableObjClass, immutableClassWithMutable, immutableObj;
+      beforeEach(function() {
+        immutableObjClass = fields.frozendict.extend({}, {
+          'key1': {'@class': fields.string},
+          'name': {'@class': fields.string}
+        });
+        immutableClassWithMutable = fields.frozendict.extend({}, {
+          'key2': {
+            '@class': fields.dictionary.extend({}, {
+              '@meta': {
+                'panelIndex': 0
+              },
+              '?': {'@class': immutableObjClass}
+            })
+          }
+        });
+        immutableObj = immutableClassWithMutable.create();
+      });
       it('are given a separate panel for each MutableObject entry', function() {
-
+        immutableObj.get('key2').push({key1: 'String_1'}, {id: 'id_1'});
+        immutableObj.get('key2').push({key1: 'String_2'}, {id: 'id_2'});
+        var panels = extractPanels(immutableObj);
+        expect(panels.length).toBe(2);
       });
 
-      it('have their title exposed via .getTitle() which mirrors `name` entry value', function() {
-
+      it('have their title exposed via .title() which mirrors `name` entry value', function() {
+        immutableObj.get('key2').push({key1: 'String_1', name: 'Name Of Panel'}, {id: 'id_1'});
+        var panels = extractPanels(immutableObj);
+        //expect(panels[0].title()).toBe('Name Of Panel');
       });
 
       it('are removable (thus are not permanent)', function() {
-
+        immutableObj.get('key2').push({key1: 'String_1'}, {id: 'id_1'});
+        var panels = extractPanels(immutableObj);
+        expect(panels[0].removable).toBe(true);
       });
 
       it('could not be spliced into one entity by giving the same `panelIndex`', function() {
-
+        // see 'are given a separate panel for each MutableObject entry'
+        // (immutableClassWithMutable have the 'panelIndex')
       })
 
     });
 
     describe('panels generated from objects other than Barricade.MutableObject (permanent panels)', function() {
+      var immutableObjClass, immutableObj;
+      beforeEach(function() {
+
+      });
       it('have fields marked with the same `panelIndex` in the one panel', function() {
+        immutableObjClass = fields.frozendict.extend({}, {
+          'key1': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 23
+              }
+            })
+          },
+          'key2': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 23
+              }
+            })
+          },
+          'key3': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 25
+              }
+            })
+          }
+        });
+        immutableObj = immutableObjClass.create();
+        var panels = extractPanels(immutableObj);
+
+        expect(panels.length).toBe(2);
+        expect(panels[0].items.panelIndex).toBe(23);
 
       });
 
       it('number of panels is defined by number of different `panelIndex` keys', function() {
+        immutableObjClass = fields.frozendict.extend({}, {
+          'key1': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 23
+              }
+            })
+          },
+          'key2': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 24
+              }
+            })
+          },
+          'key3': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 25
+              }
+            })
+          }
+        });
+        immutableObj = immutableObjClass.create();
+        var panels = extractPanels(immutableObj);
+
+        expect(panels.length).toBe(3);
 
       });
 
       it('are ordered by the `panelIndex` ascension', function() {
+        immutableObjClass = fields.frozendict.extend({}, {
+          'key1': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 25
+              }
+            })
+          },
+          'key2': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 24
+              }
+            })
+          },
+          'key3': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 23
+              }
+            })
+          }
+        });
+        immutableObj = immutableObjClass.create();
+        var panels = extractPanels(immutableObj);
+
+        expect(panels[0].items.panelIndex).toBe(23);
+        expect(panels[1].items.panelIndex).toBe(24);
+        expect(panels[2].items.panelIndex).toBe(25);
 
       });
 
       it('have no title returned from .getTitle()', function() {
-
+        immutableObjClass = fields.frozendict.extend({}, {
+          'key1': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 25
+              }
+            })
+          }
+        });
+        immutableObj = immutableObjClass.create();
+        var panels = extractPanels(immutableObj);
+        expect(panels[0].title()).toBeUndefined();
       });
 
       it('are not removable (thus are permanent)', function() {
-
+        immutableObjClass = fields.frozendict.extend({}, {
+          'key1': {
+            '@class': fields.string.extend({}, {
+              '@meta': {
+                'panelIndex': 25
+              }
+            })
+          }
+        });
+        immutableObj = immutableObjClass.create();
+        var panels = extractPanels(immutableObj);
+        expect(panels[0].removable).toBeUndefined();
       })
 
     });
